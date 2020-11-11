66de8fd285dba4266b34d1790b1fe62732c073cd
Doug Goldstein <ddgold@me.com>
2020-11-10 21:07:00 -0600

    add collapse to header

--- web/src/components/shared/header.tsx
+++ web/src/components/shared/header.tsx
@@ -9,45 +9,48 @@ export const Header = () => {
 
 	return (
 		<>
-			<Navbar bg="primary" variant="dark" style={{ marginBottom: "20px" }}>
+			<Navbar bg="primary" variant="dark" expand="lg" style={{ marginBottom: "20px" }}>
 				<Navbar.Brand as={Link} to="/">
 					Quizzing
 				</Navbar.Brand>
-				<Nav className="mr-auto">
-					{userStatus === UserStatus.LoggedIn ? (
-						<>
-							<Nav.Link as={NavLink} to="/boards/all">
-								All Boards
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="mr-auto">
+						{userStatus === UserStatus.LoggedIn ? (
+							<>
+								<Nav.Link as={NavLink} to="/boards/all">
+									All Boards
+								</Nav.Link>
+								<Nav.Link as={NavLink} to="/boards/my">
+									My Boards
+								</Nav.Link>
+								<Nav.Link as={NavLink} to="/categories/all">
+									All Categories
+								</Nav.Link>
+								<Nav.Link as={NavLink} to="/categories/my">
+									My Categories
+								</Nav.Link>
+							</>
+						) : null}
+					</Nav>
+					<Nav className="justify-content-end">
+						{userStatus === UserStatus.LoggedIn ? (
+							<Nav.Link as={NavLink} to="/user">
+								{nickname}
 							</Nav.Link>
-							<Nav.Link as={NavLink} to="/boards/my">
-								My Boards
-							</Nav.Link>
-							<Nav.Link as={NavLink} to="/categories/all">
-								All Categories
-							</Nav.Link>
-							<Nav.Link as={NavLink} to="/categories/my">
-								My Categories
-							</Nav.Link>
-						</>
-					) : null}
-				</Nav>
-				<Nav className="justify-content-end">
-					{userStatus === UserStatus.LoggedIn ? (
-						<Nav.Link as={NavLink} to="/user">
-							{nickname}
-						</Nav.Link>
-					) : null}
-					{userStatus === UserStatus.LoggedOut ? (
-						<>
-							<Nav.Link as={NavLink} to="/login">
-								Login
-							</Nav.Link>
-							<Nav.Link as={NavLink} to="/register">
-								Register
-							</Nav.Link>
-						</>
-					) : null}
-				</Nav>
+						) : null}
+						{userStatus === UserStatus.LoggedOut ? (
+							<>
+								<Nav.Link as={NavLink} to="/login">
+									Login
+								</Nav.Link>
+								<Nav.Link as={NavLink} to="/register">
+									Register
+								</Nav.Link>
+							</>
+						) : null}
+					</Nav>
+				</Navbar.Collapse>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			</Navbar>
 		</>
 	);
